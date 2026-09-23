5d302dd967af47e8abd3922c8e91de0678d700b7
MUHAMMAD ALI ARSHAD <muhammad_ali_arshad@outlook.com>
2026-09-23 12:50:29 +0500

    File Added

---
+++ home.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Muhammad Ali.");
+        System.out.println("I am learning Java.");
+        System.out.println("Java is easy!");
+    }
+}
