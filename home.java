04ad13fe9decd51ba874b273c41a4ad3afc5cb8d
MUHAMMAD ALI ARSHAD <muhammad_ali_arshad@outlook.com>
2026-09-23 12:59:25 +0500

    Rename class Main to Home

--- home.java
+++ home.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Home {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("My name is Muhammad Ali.");
