edaa441a1d9a033fe1ebb9bc09f119e464911c2d
MUHAMMAD ALI ARSHAD <muhammad_ali_arshad@outlook.com>
2026-09-23 14:10:31 +0500

    Add trigger message to Home.java

--- Home.java
+++ Home.java
@@ -4,6 +4,7 @@ public class Home {
         System.out.println("My name is Muhammad Ali.");
         System.out.println("I am learning Java.");
         System.out.println("Java is easy!");
+        System.out.println("Trigger is Working!");
     }
 }
 
